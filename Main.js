446b611c23d9e7c371934bea7e99e274fa8a7836
Guilherme Correa <guilherme100rs@gmail.com>
2023-05-15 17:27:52 -0300

    primeiro commit

---
+++ Main.js
@@ -0,0 +1,35 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+//função callback
+function handleTryclick(event) {
+  event.preventDefault()
+
+  const inputnumber = document.querySelector("#inputnumber")
+
+if (Number(inputnumber.value) == randomNumber) {
+
+  screen1.classList.add("hide")
+  screen2.classList.remove("hide")
+
+  document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} Tentativas`
+}
+inputnumber.value =""
+  xAttempts++
+  
+}
+
+//eventos
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click', handleTryclick)
+btnReset.addEventListener('click', function() {
+  
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+xAttempts=1
+})
